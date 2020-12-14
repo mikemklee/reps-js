ccc6df01f2a6475fb797561843c66fba528e5145
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-13 22:27:58 -0500

    Do some preliminary cleanup in FE

---
+++ frontend/src/Root.jsx
@@ -0,0 +1,13 @@
+import './Root.scss';
+
+function Root() {
+  return (
+    <div className='Root'>
+      <header className='Root-header'>
+        <p>Root.jsx</p>
+      </header>
+    </div>
+  );
+}
+
+export default Root;
