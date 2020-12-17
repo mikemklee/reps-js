3dbf63fe3503872f82c83c2ec66804f0d6ef360c
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-16 23:34:38 -0500

    Show Exercise cards with dummy data

--- frontend/src/views/Exercises/Exercises.jsx
+++ frontend/src/views/Exercises/Exercises.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import './Exercises.scss';
 
+import ExercisePresets from './Exercises.metadata';
+
 const Exercises = () => {
   return (
-    <div className='view-container'>
+    <div className='exercises-view'>
       <div className='view-header'>Exercises</div>
       <div className='view-content'>
-        <div className='exercises-view'></div>
+        <div className='exercise-card-container'>
+          {ExercisePresets.map((item) => (
+            <div key={item.id} className='exercise-card'>
+              <div className='exercise-card-title'>{item.name}</div>
+              <div className='exercise-card-bodypart'>
+                <span>Targets</span>
+                {item.bodyPart}
+              </div>
+              <div className='exercise-card-category'>
+                <span>Category</span>
+                {item.category}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
