db768fc6c7366d3b7f69c39f9b65e42ce8e96c03
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-20 19:43:35 -0500

    Allow ProgressRing to be rendered in the opposite direction as well

--- frontend/src/shared/ProgressRing/ProgressRing.jsx
+++ frontend/src/shared/ProgressRing/ProgressRing.jsx
@@ -7,19 +7,26 @@ const ProgressRing = ({
   radius = 10,
   strokeWidth = 2,
   progress = 0,
+  reverse = true,
   children,
 }) => {
   const strokeThickness = strokeWidth * 2;
   const normalizedRadius = radius - strokeThickness;
   const circumference = normalizedRadius * 2 * Math.PI;
-  const progressCircumerence = (progress / 100) * circumference;
+
+  const directionMultiplier = reverse ? -1 : 1;
+  const progressInFraction = (progress / 100) * directionMultiplier;
+  const progressCircumerence = progressInFraction * circumference;
   const strokeDashoffset = circumference - progressCircumerence;
 
   return (
     <div className='progress-ring'>
       <svg className='ring-container' height={radius * 2} width={radius * 2}>
         <circle
-          className='empty-ring'
+          className={classnames({
+            'static-ring': true,
+            reverse,
+          })}
           strokeWidth={strokeWidth}
           strokeDasharray={`${circumference} ${circumference}`}
           r={normalizedRadius}
@@ -28,11 +35,11 @@ const ProgressRing = ({
         />
         <circle
           className={classnames({
-            'colored-ring': true,
+            'animated-ring': true,
             animated: progress > 0,
+            reverse,
           })}
-          strokeWidth={strokeWidth}
-          strokeLinecap='round'
+          strokeWidth={strokeWidth * 1.1}
           strokeDasharray={`${circumference} ${circumference}`}
           style={{ strokeDashoffset }}
           r={normalizedRadius}
