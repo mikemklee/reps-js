7f2d5e7706740e4ae96acbf1235b6d1c57939b29
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-27 13:24:15 -0500

    FE: render cards in Routines View with exercises names for each routine

--- frontend/src/views/Routines/Routines.jsx
+++ frontend/src/views/Routines/Routines.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './Routines.scss';
 
+import ExerciseActions from '../../redux/exercise/actions';
+import RoutineActions from '../../redux/routine/actions';
+
 const Routines = () => {
+  const dispatch = useDispatch();
+  const { presets: exercisePresets, status: exerciseStatus } = useSelector(
+    (state) => state.exercise
+  );
+  const { presets: routinePresets, status: routineStatus } = useSelector(
+    (state) => state.routine
+  );
+
+  useEffect(() => {
+    dispatch(ExerciseActions.getPresetsRequest());
+    dispatch(RoutineActions.getPresetsRequest());
+  }, []);
+
+  const loading =
+    exerciseStatus.getPresetsPending || routineStatus.getPresetsPending;
+
   return (
     <div className='routines-view'>
       <div className='view-header'>Routines</div>
-      <div className='view-content'></div>
+      <div className='view-content'>
+        {loading ? (
+          'loading'
+        ) : (
+          <div className='routines'>
+            {routinePresets.map((item) => (
+              <div key={item._id} className='card'>
+                <div className='card__title'>{item.name}</div>
+                <div className='card__exerciseList'>
+                  {item.exercises.map((exercise) => {
+                    const exerciseData = exercisePresets.find(
+                      (item) => item._id === exercise.presetId
+                    );
+                    return (
+                      <div
+                        key={exercise.presetId}
+                        className='card__exerciseItem'
+                      >
+                        {exerciseData.name}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
