74041bbf7e3155b176f7938bc919b1e4ef003775
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-28 11:59:19 -0500

    FE: modify styles for cards in Exercise view; show "tags" for target muscles and exercise category

--- frontend/src/views/Exercises/Exercises.jsx
+++ frontend/src/views/Exercises/Exercises.jsx
@@ -17,17 +17,26 @@ const Exercises = () => {
     <div className='exercises-view'>
       <div className='view-header'>Exercises</div>
       <div className='view-content'>
-        <div className='exercise-card-container'>
+        <div className='exerciseCardList'>
           {presets.map((item) => (
-            <div key={item._id} className='exercise-card'>
-              <div className='exercise-card-title'>{item.name}</div>
-              <div className='exercise-card-bodypart'>
-                <span>Targets</span>
-                {item.bodyPart}
+            <div key={item._id} className='exerciseCard'>
+              <div className='exerciseCard__title'>{item.name}</div>
+              <div className='exerciseCard__details'>
+                <span className='exerciseCard__detailKey'>Targets</span>
+                {item.bodyParts.map((part, index) => (
+                  <span
+                    key={index}
+                    className='exerciseCard__detailValue exerciseCard__detailValue--bodypart'
+                  >
+                    {part}
+                  </span>
+                ))}
               </div>
-              <div className='exercise-card-category'>
-                <span>Category</span>
-                {item.category}
+              <div className='exerciseCard__details'>
+                <span className='exerciseCard__detailKey'>Category</span>
+                <span className='exerciseCard__detailValue exerciseCard__detailValue--category'>
+                  {item.category}
+                </span>
               </div>
             </div>
           ))}
