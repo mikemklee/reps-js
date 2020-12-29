1358cac9028c68b09a68c91f230167c8e5c0bf76
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-29 09:57:48 -0500

    FE: refactor JSX and styles in QuickActions

--- frontend/src/views/Main/QuickActions/QuickActions.jsx
+++ frontend/src/views/Main/QuickActions/QuickActions.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
 import { GiWeightLiftingUp } from 'react-icons/gi';
 import { HiOutlineClipboardList } from 'react-icons/hi';
@@ -9,29 +9,43 @@ import './QuickActions.scss';
 const QuickActions = () => {
   const history = useHistory();
 
+  const cards = useMemo(() => [
+    {
+      title: 'Start a blank workout',
+      icon: <GiWeightLiftingUp />,
+      route: '/workout/new',
+    },
+    {
+      title: 'View routines',
+      icon: <BiAnalyse />,
+      route: '/routines',
+    },
+    {
+      title: 'View exercises',
+      icon: <BiDumbbell />,
+      route: '/exercises',
+    },
+    {
+      title: 'View logs',
+      icon: <HiOutlineClipboardList />,
+      route: '/logs',
+    },
+  ]);
+
   return (
-    <div className='quick-actions'>
-      <label className='section-title'>Quick actions</label>
-      <div className='action-card-container'>
-        <div
-          className='action-card'
-          onClick={() => history.push('/workout/new')}
-        >
-          <label className='action-card-title'>Start a blank workout</label>
-          <GiWeightLiftingUp className='action-card-icon' />
-        </div>
-        <div className='action-card' onClick={() => history.push('/routines')}>
-          <label className='action-card-title'>View routines</label>
-          <BiAnalyse className='action-card-icon' />
-        </div>
-        <div className='action-card' onClick={() => history.push('/exercises')}>
-          <label className='action-card-title'>View exercises</label>
-          <BiDumbbell className='action-card-icon' />
-        </div>
-        <div className='action-card' onClick={() => history.push('/logs')}>
-          <label className='action-card-title'>View logs</label>
-          <HiOutlineClipboardList className='action-card-icon' />
-        </div>
+    <div className='quickActions'>
+      <label className='quickActions__title'>Quick actions</label>
+      <div className='actionCardList'>
+        {cards.map((card) => (
+          <div
+            key={card.route}
+            className='actionCard'
+            onClick={() => history.push(card.route)}
+          >
+            <label className='actionCard__title'>{card.title}</label>
+            <div className='actionCard__button'>{card.icon}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
