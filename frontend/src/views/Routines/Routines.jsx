b23af3ec0496e5cda898b99876d3bcb86aeb5674
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-30 07:12:41 -0500

    FE: add a section for custom routines in Routines view

--- frontend/src/views/Routines/Routines.jsx
+++ frontend/src/views/Routines/Routines.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { VscAdd } from 'react-icons/vsc';
 import _ from 'lodash';
 
 import './Routines.scss';
@@ -33,15 +34,30 @@ const Routines = () => {
         {loading ? (
           <LoadingSpinner />
         ) : (
-          <div className='routines'>
-            {_.map(routinePresets, (item) => (
-              <RoutineCard
-                key={item._id}
-                routine={item}
-                exerciseNames={exerciseNames}
-              />
-            ))}
-          </div>
+          <>
+            <div className='customRoutines'>
+              <div className='customRoutines__title'>Custom routines</div>
+              <div className='customRoutines__addNew'>
+                Add new custom routine
+                <div className='customRoutines__addNew__icon'>
+                  <VscAdd />
+                </div>
+              </div>
+              <div className='customRoutines__routines'></div>
+            </div>
+            <div className='presetRoutines'>
+              <div className='presetRoutines__title'>Preset routines</div>
+              <div className='presetRoutines__routines'>
+                {_.map(routinePresets, (item) => (
+                  <RoutineCard
+                    key={item._id}
+                    routine={item}
+                    exerciseNames={exerciseNames}
+                  />
+                ))}
+              </div>
+            </div>
+          </>
         )}
       </div>
     </div>
