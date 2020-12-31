aa5f72755ed8c0e9ab3b899644885d1a84227c90
Mike Lee <mikemunkyu.lee@gmail.com>
2020-12-31 18:07:32 -0500

    BE: no-op protect middleware; conflicts with rest of authentication flow

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,37 +3,40 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const protect = async (req, res, next) => {
-  let token;
-
-  const authHeader = req.headers.authorization;
-  if (authHeader && authHeader.startsWith('Bearer')) {
-    try {
-      token = authHeader.split(' ')[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.id).select('-password');
-
-      // TODO: error out if no user is found
-
-      next();
-      return;
-    } catch (err) {
-      console.error(err);
-      res.status(401).json({
-        message: 'Not authorized. Token verification failed.',
-      });
-      return;
-    }
-  }
-
-  if (!token) {
-    res.status(401).json({
-      message: 'Not authorized. No token found.',
-    });
-    return;
-  }
-
+  // TODO: use this middleware when supporting users with email/password authentication?
+  // TODO: no-op for now
   next();
+  // let token;
+
+  // const authHeader = req.headers.authorization;
+  // if (authHeader && authHeader.startsWith('Bearer')) {
+  //   try {
+  //     token = authHeader.split(' ')[1];
+  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  //     req.user = await User.findById(decoded.id).select('-password');
+
+  //     // TODO: error out if no user is found
+
+  //     next();
+  //     return;
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(401).json({
+  //       message: 'Not authorized. Token verification failed.',
+  //     });
+  //     return;
+  //   }
+  // }
+
+  // if (!token) {
+  //   res.status(401).json({
+  //     message: 'Not authorized. No token found.',
+  //   });
+  //   return;
+  // }
+
+  // next();
 };
 
 module.exports = { protect };
