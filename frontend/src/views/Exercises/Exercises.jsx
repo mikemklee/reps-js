0e54398a2e058ce9d1f11976fd9c300600fcea5a
Mike Lee <mikemunkyu.lee@gmail.com>
2021-01-01 16:48:44 -0500

    FE: add a search bar component in Exercises view

--- frontend/src/views/Exercises/Exercises.jsx
+++ frontend/src/views/Exercises/Exercises.jsx
@@ -1,24 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { FiSearch } from 'react-icons/fi';
+import _ from 'lodash';
 
 import './Exercises.scss';
 
-import ExerciseActions from '../../redux/exercise/actions';
 import { ExerciseCardList } from '../../shared';
 
+import ExerciseActions from '../../redux/exercise/actions';
+
+import usePrevious from '../../hooks/usePrevious';
+
 const Exercises = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   const dispatch = useDispatch();
-  const { presets } = useSelector((state) => state.exercise);
+  const { presets, status: exerciseStatus } = useSelector(
+    (state) => state.exercise
+  );
+  const prevExerciseStatus = usePrevious(exerciseStatus);
+
+  const onChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  useEffect(() => {
+    const results = _.filter(presets, (item) =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
 
   useEffect(() => {
     dispatch(ExerciseActions.getPresetsRequest());
   }, []);
 
+  useEffect(() => {
+    if (prevExerciseStatus && exerciseStatus) {
+      if (
+        !prevExerciseStatus.getPresetsSuccess &&
+        exerciseStatus.getPresetsSuccess
+      ) {
+        setSearchResults(_.map(presets));
+      }
+    }
+  }, [exerciseStatus, prevExerciseStatus]);
+
   return (
-    <div className='exercises-view'>
+    <div className='exercisesView'>
       <div className='view-header'>Exercises</div>
       <div className='view-content'>
-        <ExerciseCardList presets={presets} />
+        <div className='exercisesView__searchBar'>
+          <input
+            type='text'
+            placeholder='Search'
+            value={searchTerm}
+            onChange={onChange}
+          />
+          <FiSearch className='exercisesView__searchBar__icon' />
+        </div>
+        <ExerciseCardList
+          presets={searchResults}
+          title='Preset exercises'
+          placeholder='No matching exercises found.'
+        />
       </div>
     </div>
   );
