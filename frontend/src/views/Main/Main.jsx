83fb719a0dc02f626320f5d6c74954b3a40f10d5
Mike Lee <mikemunkyu.lee@gmail.com>
2021-01-02 23:30:27 -0500

    FE: render Calendar in Main view

--- frontend/src/views/Main/Main.jsx
+++ frontend/src/views/Main/Main.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import './Main.scss';
+
 import QuickActions from './QuickActions/QuickActions';
+
+import { Calendar } from '../../shared';
 
 import { InterfaceUtils } from '../../utils';
 
 const Main = () => {
   const { userDisplayName } = useSelector((state) => state.auth);
+
   return (
-    <div className='main-view'>
-      <div className='view-header'>
+    <div className='mainView'>
+      <div className='mainView__header'>
         Good {InterfaceUtils.getTimeOfDay()}, <strong>{userDisplayName}</strong>
       </div>
-      <div className='view-content'>
-        <QuickActions />
+      <div className='mainView__content'>
+        <div className='mainView__calendar'>
+          <label className='mainView__calendar__title'>Monthly progress</label>
+          <Calendar />
+        </div>
+        <div className='mainView__quickActions'>
+          <QuickActions />
+        </div>
       </div>
       {/* <div className='weekly-progress'>TODO: Show weekly progress</div> */}
       {/* <div className='workout-curation'>TODO: Show workout curation</div> */}
