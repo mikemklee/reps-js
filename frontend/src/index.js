48b8bd1abb8fbe731ec1c8d8b4c56ba75df89e91
Mike Lee <mikemunkyu.lee@gmail.com>
2021-01-06 09:04:37 -0500

    FE: setup MUI at index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
 import './index.scss';
 import AppRouter from './router/Router.jsx';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
+// define MUI theme
+const defaultMaterialTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0078ff',
+    },
+    background: {
+      default: '#21252b',
+      paper: '#282c34',
+    },
+    text: {
+      primary: '#b5b6c4',
+    },
+  },
+  typography: {
+    fontSize: 12,
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Open Sans',
+      'Noto Sans KR',
+    ],
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRouter />
+      <ThemeProvider theme={defaultMaterialTheme}>
+        <AppRouter />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
