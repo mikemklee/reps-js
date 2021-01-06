fae4ed8d27bae4bc5c6031e7856bf9ca0544a34f
Mike Lee <mikemunkyu.lee@gmail.com>
2021-01-05 22:51:26 -0500

    FE: add MUI datetime picker and apply some basic stylings

--- frontend/src/views/Logs/WorkoutCard/WorkoutMeta/WorkoutMeta.jsx
+++ frontend/src/views/Logs/WorkoutCard/WorkoutMeta/WorkoutMeta.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BiTimeFive, BiCalendarCheck } from 'react-icons/bi';
 import { FaWeightHanging } from 'react-icons/fa';
@@ -6,11 +6,41 @@ import classnames from 'classnames';
 import moment from 'moment';
 import _ from 'lodash';
 
+// MUI
+import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import DateFnsUtils from '@date-io/date-fns';
+
 import './WorkoutMeta.scss';
 
 import { useExercises, useWeightConverter } from '../../../../hooks';
 
+// define MUI theme
+const defaultMaterialTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0078ff',
+    },
+    background: {
+      default: '#21252b',
+      paper: '#282c34',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Open Sans',
+      'Noto Sans KR',
+    ],
+  },
+});
+
 const WorkoutMeta = ({ item, vertical = false, showVolume = false }) => {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   const { currentWeightUnit, computeDisplayedWeight } = useWeightConverter();
   const { categoryNames } = useExercises();
 
@@ -46,25 +76,35 @@ const WorkoutMeta = ({ item, vertical = false, showVolume = false }) => {
   );
 
   return (
-    <div className={classnames('workoutMeta', { vertical })}>
-      <div className='workoutMeta__completedAt'>
-        <BiCalendarCheck />
-        <span>{formattedCompletedAt}</span>
-      </div>
-      <div className='workoutMeta__duration'>
-        <BiTimeFive />
-        <span>{formattedDuration} minutes</span>
-      </div>
-      {showVolume ? (
-        <div className='workoutMeta__volume'>
-          <FaWeightHanging />
-          <span>
-            {Number(computeDisplayedWeight(totalVolumeInKG).toFixed(2))}{' '}
-            {currentWeightUnit}
-          </span>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <div className={classnames('workoutMeta', { vertical })}>
+        <ThemeProvider theme={defaultMaterialTheme}>
+          <DateTimePicker
+            className='workoutMeta__dateAndTime'
+            value={selectedDate}
+            format='h:mm a EEE, d MMM yyyy'
+            onChange={handleDateChange}
+          />
+        </ThemeProvider>
+        {/* <div className='workoutMeta__completedAt'>
+          <BiCalendarCheck />
+          <span>{formattedCompletedAt}</span>
+        </div> */}
+        <div className='workoutMeta__duration'>
+          <BiTimeFive />
+          <span>{formattedDuration} minutes</span>
         </div>
-      ) : null}
-    </div>
+        {showVolume ? (
+          <div className='workoutMeta__volume'>
+            <FaWeightHanging />
+            <span>
+              {Number(computeDisplayedWeight(totalVolumeInKG).toFixed(2))}{' '}
+              {currentWeightUnit}
+            </span>
+          </div>
+        ) : null}
+      </div>
+    </MuiPickersUtilsProvider>
   );
 };
 
