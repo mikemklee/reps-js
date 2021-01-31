5bdfde92c57e863979ef631b22ffd30e2965b725
Mike Lee <mikemunkyu.lee@gmail.com>
2021-01-30 22:20:13 -0500

    FE: styling and UX tweaks

--- frontend/src/shared/NumberInput/NumberInput.jsx
+++ frontend/src/shared/NumberInput/NumberInput.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import './NumberInput.scss';
 
 const NumberInput = ({ value = 0, onChange = () => {}, onBlur = () => {} }) => {
+  const handleFocus = (event) => event.target.select();
+
   return (
     <input
+      onFocus={handleFocus}
       className='numberInput'
       type='tel'
       value={value}
